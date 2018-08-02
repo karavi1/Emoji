436e46fb43392d731ea93e76a99ba2816831b7da
Kaushik Ravi <kaush.ravi@gmail.com>
2018-08-02 09:57:19 -0700

    App

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -20,6 +20,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func showMessage(sender: UIButton) {
+        
+        // Initialize the dictionary for the emoji icons
+        // If you forgot how to do it, refer to the previous chapter
+        // Fill in the code below
+        
+        var emojiDict = ["👻": "Ghost", "🤖": "Robot", "😤": "Angry", "🤓": "Nerdy", "👾": "Alien monster"]
+        
+        
+        // The sender is the button that is tapped by the user.
+        // Here we store the sender in the selectedButton constant
+        let selectedButton = sender
+        
+        // Get the emoji from the title label of the selected button
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            
+            // Get the meaning of the emoji from the dictionary
+            // Fill in the code below
+            
+            let meaning = emojiDict[wordToLookup]
+            
+            // Change the line below to display the meaning of the emoji instead of Hello World
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertControllerStyle.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        
+    }
 
 }
 
